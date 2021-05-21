62689a66fd68fbe7229d801d557cbcfdc8cc30de
Jorge Benavides <jorgebenaperez@gmail.com>
2021-05-20 23:18:31 -0500

    cambios

--- Villaplatzi/platzi.js
+++ Villaplatzi/platzi.js
@@ -1,31 +1,40 @@
 var vp = document.getElementById("villaplatzi");
 var papel = vp.getContext("2d");
-var mapa = "tile.png";
 
 var fondo = {
-  url: "fondo.png",
-  cargaOK: false
+  url: "tile.png",
+  cargaOK: false,
 };
-
 var vaca = {
   url: "vaca.png",
-  cargaOK: false
+  cargaOK: false,
 };
 
-var fondo.imagen = new Image();
-fondo.imagen.src = mapa;
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
 
 vaca.imagen = new Image();
-vaca.imagen.src = "vaca.png";
+vaca.imagen.src = vaca.url;
 vaca.imagen.addEventListener("load", cargarVacas);
 
 function cargarFondo() {
   fondo.cargaOK = true;
+  dibujar();
+}
+function cargarVacas() {
+  vaca.cargaOK = true;
+  dibujar();
 }
 
 function dibujar() {
-  papel.drawImage(fondo, 0, 0);
+  if (fondo.cargaOK) {
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+}
+
+if (vaca.cargaOK) {
+  papel.drawImage(vaca.imagen, 0, 0);
 }
 
 function aleatorio(min, maxi) {
